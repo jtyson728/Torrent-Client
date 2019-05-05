62f398ecdbddb60266e4a6fb11f4621af20883dd
Jeremy L. Tyson <jlt179@cp.cs.rutgers.edu>
2019-05-05 18:38:21 -0400

    boiler

--- client/clienttest.py
+++ client/clienttest.py
@@ -1,9 +1,11 @@
 import xmlrpc.client
 
-with xmlrpc.client.ServerProxy("http://null.cs.rutgers.edu:40000/") as proxy:
+with xmlrpc.client.ServerProxy("http://null.cs.rutgers.edu:8000/") as proxy: 
     print(proxy)
-    while 1:
-        cmd = input("Next command \n")
-    if "respond" in cmd:
-        msg = cmd.replace("respond ", "")
-        print(proxy.respond(msg))
+#    while 1:
+#        cmd = input("Next command \n")
+#    if "respond" in cmd:
+#        msg = cmd.replace("respond ", "")
+#        print(proxy.respond(msg))
+
+    print(proxy.start_daemon)
