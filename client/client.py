82e5567accc5c82288b7678f1838a8f7ece3125b
hariamoor <amoor.hari@gmail.com>
2019-05-09 19:09:35 -0400

    resolved client conflict

--- client/client.py
+++ client/client.py
@@ -13,7 +13,6 @@ proxy = xmlrpc.client.ServerProxy("http://{}:{}".format(config.SERVER_URI, confi
 
 def add(args):
     hash_keys = proxy.add(args.filenames)
-<<<<<<< HEAD
     if isinstance(hash_keys, Exception):
         print("Exception occurred: {}".format(hash_keys))
     else:
@@ -25,14 +24,11 @@ def remove(args):
         print("Exception occurred: {}".format(paths))
     else:
         print("The torrents at paths {} with hash keys {} has been removed.".format(paths, args.hash_keys))
-=======
-    print("The hash keys for the torrents stored at {} (respectively) are: {}.".format(args.filenames, hash_keys))
 
 def remove(args):
     print(args)
     paths = proxy.remove(args.hash_keys)
     print("The torrents at paths {} with hash keys {} has been removed.".format(paths, args.hash_keys))
->>>>>>> b6014610b2dbb9b74236700d1dd3674d6f290096
 
 def main():
     parser = argparse.ArgumentParser(description="A client for the torrent RPC server (CLI)")
@@ -51,11 +47,7 @@ def main():
             "filenames",
             nargs="+",
             help="Torrent file names")
-<<<<<<< HEAD
-=======
 
-    subparser.set_defaults(func=partial(run, add))
->>>>>>> b6014610b2dbb9b74236700d1dd3674d6f290096
     subparser.set_defaults(func=add)
 
     subparser = subparsers.add_parser(
@@ -66,15 +58,10 @@ def main():
             "hash_keys",
             nargs="+",
             help="Hash keys of torrents to remove")
-<<<<<<< HEAD
-=======
 
-    subparser.set_defaults(func=partial(run, remove))
->>>>>>> b6014610b2dbb9b74236700d1dd3674d6f290096
     subparser.set_defaults(func=remove)
 
     arguments = parser.parse_args()
-
     try:
         # try to run appropriate function
         arguments.func(arguments)
